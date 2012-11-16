42d00334534c97df54ea1b52b7a78515cbb7045e
SevenBits <RyanBowring@Ryans-MacBook-2.local>
2012-11-16 17:37:27 -0500

    Initial Commit

---
+++ Mac Linux USB Loader/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Mac Linux USB Loader
+//
+//  Created by Ryan Bowring on 11/16/12.
+//  Copyright (c) 2012 Ryan Bowring. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Mac Linux USB Loader/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Mac Linux USB Loader
+//
+//  Created by Ryan Bowring on 11/16/12.
+//  Copyright (c) 2012 Ryan Bowring. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
